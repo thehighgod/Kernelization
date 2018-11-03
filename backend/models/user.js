b9da6f303c938d4d21bd13598656512bcde2dcf4
Brett Broadhurst <42479242+brettbroadhurst@users.noreply.github.com>
2018-11-03 00:31:59 -0700

    Added email regex.

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,7 +6,12 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
-	email: {type: String, required: true, unique: true},
+	email: {
+		type: String, 
+		required: true, 
+		unique: true,
+		match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+	},
 	username: {type: String, required: true},
 	password: {type: String, required: true}
 });
